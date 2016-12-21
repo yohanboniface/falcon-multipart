d389e17e4e404e888eb494c21601b1f074c31c27
saboten <saboten@192.168.0.220>
2016-12-21 10:46:30 +0900

    fix for Falcon 1.0.0 and 1.1.0  this fix should also work for 0.3.0

--- falcon_multipart/middleware.py
+++ falcon_multipart/middleware.py
@@ -17,7 +17,7 @@ class MultipartMiddleware(object):
         # This must be done to avoid a bug in cgi.FieldStorage.
         req.env.setdefault('QUERY_STRING', '')
 
-        form = self.parse(stream=req.stream, environ=req.env)
+        form = self.parse(stream=req.stream.stream, environ=req.env)
         for key in form:
             field = form[key]
             if not getattr(field, 'filename', False):
